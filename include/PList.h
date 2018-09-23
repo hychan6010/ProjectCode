cf535f9abdf155bccfa1f0c505ac058ea4ffb0a3
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-09-23 19:28:41 -0400

    Create PList.h

---
+++ include/PList.h
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stddef.h>
+
+class Object {
+public:
+  virtal void print();
+ };
+ 
+ class String : public Object {
+ public:
+        const char* str;
+        String(const char *s = "") str(s) {}
+        void print();
+  };
+  
+  class Integer : public Object {
+  public:
+        int x;
+        Integer(int i = 0): x(i) {}
+        void print();
+   };
+   
+   class Double : public Object {
+   public:
+          double a;
+          Double(double ii = 0.0) : a(ii) {}
+          void print();
+     };
+     
+ class List {
+  private:
+          class ListNode{
+          private:
+                 object object;
+                 ListNode *next;
+           public: 
+                   List Node(Object o, ListNode *n = NULL){
+                          object = o;
+                          next = NULL;
+                     }
+                     ListNode = *getNext(){
+                            return next;
+                      }
+                      void setNext(ListNode *n){
+                             next = n;
+                        }
+                        Object getObject () {
+                        return object;
+                        }
+                      };
+                      ListNode *head;
+                      ListNode *tail;
+                      
+                      public
+                          class iterator {
+                          private:
+                          ListNode *node;
+                          
+                          public
+                          Iterator(ListNode *n=NULL){
+                                    node = n;
+                           }
+                           Object &getObject(){
+                                    return node -> getObject();
+                            }
+                                    void increment (){
+                                     node = node ->getNext();
+                             bool end(){
+                                        return node = NULL;
+                                        }
+                             
+                             };
+                             
+                             List();
+                             void append(Object o);
+                             bool remove(Object &o);
+                             void removeAfter(iterator it);
+                             void insertAfter(iterator it, Object object);
+                             bool empty();
+                             iterator begin()
+                             {
+                             return iterator(head);
+                             }
+                             ~List();
+                             };
+                                    
+              
+      
