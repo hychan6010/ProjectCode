d54818a81094fb09c2cb6fa59c3ca69646e3da48
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-09-26 19:19:06 -0400

    Create Test.cpp

---
+++ tests/Test.cpp
@@ -0,0 +1,23 @@
+int main()
+{
+    OList o;
+    
+    String words ("Hello");
+    Integer number(5);
+    Double double(2.5);
+    o.append(words);
+    o.append(number);
+    o.append(double);
+    
+    
+    List::iterator it = o.begin();
+  while(!it.end())
+    {
+      Object &object = it.getObject();
+
+   object->print();
+
+      it.increment();
+}
+
+    }
