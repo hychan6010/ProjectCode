618b5fd34f23ff3641d80e488362e0a77d83ddd6
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-09-26 15:59:41 -0400

    Update PList.h

--- include/PList.h
+++ include/PList.h
@@ -44,7 +44,7 @@ public:
                       void setNext(ListNode *n){
                              next = n;
                         }
-                        Object getObject () {
+                        Object& getObject () {
                         return object;
                         }
                       };
