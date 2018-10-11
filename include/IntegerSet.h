7cef81b9a71ef62502d8119389235e6d7f3867d1
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-10-10 21:35:29 -0400

    Update IntegerSet.h

--- include/IntegerSet.h
+++ include/IntegerSet.h
@@ -15,4 +15,35 @@ public:
    virtual void remove(int) = 0;
 };
 
-int get
+IntegerSet::IntegerSet(int htsize)
+{
+   table = new int[size];
+   for(int i=0; i<size; i++)
+      table[i] = -1;  // -1 means empty
+}
+
+bool IntegerSet::insert(int data)
+{
+   if ( table[hash(data)] == -1)
+   {
+     // if the entry is not being used, put the
+    // data there
+     table[ hash(data) ] = data;
+     return true;
+   }
+  // otherwise give up
+   return false;
+}
+
+bool IntegerSet::search(int data) const
+{
+  // data can only be in one location, check it
+  return table[hash(data)]==data;
+}
+
+void IntegerSet::remove(int data)
+{
+   if ( table[hash(data)] == data )
+       table[hash(data)] = -1;
+}
+
