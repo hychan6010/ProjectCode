7a07dce5b3b2dd2324d5a8f3bad7dd6f82453c99
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-10-11 12:39:02 -0400

    Update IntArray.h

--- include/IntArray.h
+++ include/IntArray.h
@@ -9,3 +9,39 @@ private:
    int *array;
    int length;
 public:
+      IntArray(int le, int val=0):length(le) {
+      array = new int[le];
+      for(int i=0; i<le; i++)
+        array[i] = val;
+   }
+
+   ~IntArray() {
+     delete [] array;
+}
+   
+Int Array(int size, int val=0){
+   array = new int[size];
+   for(int i = 0; i < size; i++){
+      array[i] = val;
+   }
+}
+   
+   
+   int* operator[](int index){
+      return array[index];
+   }
+      
+      
+   
+IntArray& operator+=(const IntArray &rhs) {
+   if(this == &rhs) {
+      return *this;
+   }
+      printf("Running operator=!\n");
+      delete [] array;
+      length = rhs.length;
+      array = new int[length];
+         for(int i=0; i<length; i++)
+            array[i] = rhs.array[i];
+            return *this; // reference to this object
+}
