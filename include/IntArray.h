93a5b2c3a1c79f69606b117acb72068976460d86
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-10-12 21:11:31 -0400

    Update IntArray.h

--- include/IntArray.h
+++ include/IntArray.h
@@ -20,119 +20,142 @@ public:
      delete [] array;
 }
 
-IntArray(const char* str) {
-   length = strlen(str)
-   array = (int)(str)
-   for(int i = 0; i < length; i++){
-         array[i];
-   }
-}
-   
-
-            
-Int Array(int size, int val=0){
+IntArray::IntArray(const char* str) {
+   size=asize;
    array = new int[size];
-   for(int i = 0; i < size; i++){
-      array[i] = val;
+   for(int a = 0; a<size;a++){
+      array[a]=str[a];
    }
 }
    
-   
-   int* operator[](int index){
-      return array[index];
+            
+Int Array(int asize, int val=0){
+   size = asize;
+   array = new int[size]
+      for(int a = 0; a<size;a++){
+         array[a]=val;
    }
-      
-      
-   
-IntArray& operator+=(const IntArray &rhs) {
-   if(this == &rhs) {
-      return *this;
-   }
-      printf("Running operator=!\n");
-      delete [] array;
-      length = rhs.length;
-      array = new int[length];
-         for(int i=0; i<length; i++)
-            array[i] = rhs.array[i];
-            return *this; // reference to this object
-}
-      
-IntArray IntArray::operator + (const IntArray &rhs) const
-{
-  IntArray result(length); // make object to hold return value,
-  	                       // make long enough to hold this object’s data
-    int i=0;
-    for(i=0; i<length && i<rhs.length; i++)
-       result.array[i] = array[i] + rhs.array[i];
-    for(;i<length;i++) // if rhs.length is shorter, copy rest of array
-       result.array[i] = array[i];
-    return result;
-}
-IntArray IntArray::operator - (const IntArray &rhs) const
-{
-  IntArray result(length); // make object to hold return value,
-  	                       // make long enough to hold this object’s data
-    int i=0;
-    for(i=0; i<length && i<rhs.length; i++)
-       result.array[i] = array[i] - rhs.array[i];
-    for(;i<length;i++) // if rhs.length is shorter, copy rest of array
-       result.array[i] = array[i];
-    return result;
-}
-      
-IntArray IntArray::operator * (const IntArray &rhs) const
-{
-  IntArray result(length); // make object to hold return value,
-  	                       // make long enough to hold this object’s data
-    int i=0;
-    for(i=0; i<length && i<rhs.length; i++)
-       result.array[i] = array[i] * rhs.array[i];
-    for(;i<length;i++) // if rhs.length is shorter, copy rest of array
-       result.array[i] = array[i];
-    return result;
 }
 
-IntArray IntArray::operator / (const IntArray &rhs) const
-{
-  IntArray result(length); // make object to hold return value,
-  	                       // make long enough to hold this object’s data
-    int i=0;
-    for(i=0; i<length && i<rhs.length; i++)
-       result.array[i] = array[i] / rhs.array[i];
-    for(;i<length;i++) // if rhs.length is shorter, copy rest of array
-       result.array[i] = array[i];
-    return result;
-}      
+int IntArray:: get(int index)const{
+   return array[index];
+}
       
-IntArray IntArray::operator << (const IntArray &rhs) const
-{
-  IntArray result(length); // make object to hold return value,
-  	                       // make long enough to hold this object’s data
-    int i=0;
-    for(i=0; i<length && i<rhs.length; i++)
-       result.array[i] = array[i] - rhs.array[i];
-    for(;i<length;i++) 
-       result.array[i] = array[i];
-    return result;
-}      
-
-   
-IntArray IntArray:: operator char*(const IntArray &rhs) const
-{
-   IntArray 
-      
-      
-      
+int IntArray::getLength() const{
+   return size;
 }
    
-IntArray IntArray:: operator int(const IntArray) const
-{
-       int i=0;
-       int j=0;
-       int average = 0;
-    for(i=0; i<length; i++){
-       array[i]+=j;
+void IntArray::set(int index, int value){
+   array[index] = value;
+}
+int& IntArray::operator[](int index){
+   return array[index];
+}
+   
+IntArray& IntArray::operator+=(const IntArray &rhs) {
+   int tempsize; 
+   if(rhs.getLength()<size){
+      tempsize = rhs.getLength();
+   }
+   else{
+         tempsize = size;
+   }
+   for(int a=0; a<tempsize; a++){
+      array[a]+=rhs.get(a);
+   }
+   return *this;
+}
+IntArray IntArray::operator+(const IntArray &rhs){
+   int tempsize;
+   if(rhs.getLength()<size){
+      tempsize=rhs.getLength();
+   }
+   else{
+         tempsize=size;
+   }
+   IntArray temparray(size);
+   for(int a=0;a<tempsize;a++){
+      temparray.set(a,array[a]+rhs.get(a));
+   }
+   if(tempsize != size){
+      for(int b = tempsize; b<size; b++){
+         temparray.set(b,array[b]);
+      }
+   }
+   return temparray;
+}
+   
+   IntArray IntArray::operator-(const IntArray &rhs){
+      int tempsize;
+      if(rhs.getLength()<size){
+         tempsize=rhs.getLength();
+      }
+      else{
+         tempsize=size;
+      }
+      IntArray temparray(size)
+         for(int a=0;tempsize;a++){
+            temparray.set(a,array[a]-rhs.get(a));
+         }
+      if(tempsize!=size){
+         for(int b=tempsize;b<size;b++){
+            temparray.set(b, array[b]);
+         }
+      }
+      return temparray;
+   }
+   IntArray IntArray::operator*(int x){
+      IntArray temparray(size);
+      for(int a=0;a<size;a++){
+         temparray.set(a,array[a]*x);
+      }
+      return temparray;
+   }
+   
+   IntArray IntArray::operator/(int x){
+      IntArray temparray(size);
+      for(int a=0;a<size;a++){
+         temparray.set(a,array[a]/x);
+      }
+      return temparray;
+   }
+   IntArray IntArray::operator<<(int count)const{
+      int temp;
+      IntArray temparray(size);
+      for(int a=0;a<size;a++){
+         if(a+count>=size){
+            temp=a+count-size;
+         }
+         else{
+            temp=a+count;
+         }
+         temparray.set(temp,array[a]);
+      }
+      return temparray;
+   }
+   IntArray::operator char*()const{
+      char *str=new char[size+1];
+      int count=0;
+      for(int a =0;a<size;a++){
+         if(array[a]<0 || array[a]>255){
+            str[a] = ' ';
+         }
+         else if(array[a]==0){
+            count = 1;
+         }
+         str[a]=(char)array[a];
+      }
+   }
+   if(count==0){
+      str[size]='\0';
+   }
+   return str;
+}
+IntArray::operator int() const{
+   int total = 0;
+    for(int a=0; a<size;a++){
+       total+= array[a];
     }
-      (j/ length)=average;
-      return average;
+   return total=total/size;
 }
+
