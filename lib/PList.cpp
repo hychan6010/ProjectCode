1b6d9c19eb5cae041f1fe16679ea22fecf09a68c
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-10-26 19:08:52 -0400

    Update PList.cpp

--- lib/PList.cpp
+++ lib/PList.cpp
@@ -2,6 +2,7 @@
 
 PList::PList()
 {
+
   head = NULL;
   tail = NULL;
 }
@@ -10,7 +11,7 @@ void PList::append(Object *o)
 {
   ListNode *node = new ListNode(o);
   if (head == NULL)
-    {
+    
       head = node;
       tail = node;
     }
@@ -25,15 +26,15 @@ bool PList::remove(Object*& copy)
 {
   if (!empty())
     {
-      copy = head->getObject();
+      copy = head->getObject(); 
       ListNode *tmp = head->getNext();
       delete head; 
       head = tmp; 
-      if (tmp==NULL)
+      if (tmp==NULL) 
 	tail = NULL;
       return true;
     }
-  return false;
+  return false; 
 }
 
 bool PList::empty()
@@ -41,11 +42,30 @@ bool PList::empty()
   return head==NULL;
 }
 
-void PList::insertAfter(PList::iterator it, Object *o)
+void PList::removeAfter(PList::iterator it) 
+{
+   if (it.node==NULL) 
+     {
+       ListNode *remove = head;
+       head = head->getNext(); 
+       if (head==NULL) tail = NULL; 
+       delete remove; 
+     }
+   else 
+     {
+       ListNode *tmp = it.node;
+       ListNode *remove = tmp->getNext(); 
+       tmp->setNext(remove->getNext());  
+       if (tail==remove) tail = tmp;    
+       delete remove; 
+     }
+}
+
+void PList::insertAfter(PList::iterator it, Object *o) 
 {
     if (it.node==NULL)
       { 
-	
+
 	ListNode *node = new ListNode(o,head);
 	if (head==NULL) { 
 	  tail = node; 
@@ -54,57 +74,11 @@ void PList::insertAfter(PList::iterator it, Object *o)
 	  head = node; 
 	}
       }
-    else
+    else 
     {
 	     ListNode *node = new ListNode(o,it.node->getNext());
 	     it.node->setNext(node);
 	     if (tail==it.node) tail = node;
     }
 }
-
-void PList::removeAfter(PList::iterator it) 
-{
-   if (it.node==NULL)
-     {
-       ListNode *remove = head;              
-       head = head->getNext();  
-       if (head==NULL) tail = NULL;
-       delete remove;              
-     }
-   else 
-     {
-       ListNode *tmp = it.node;
-       ListNode *remove = tmp->getNext();
-       tmp->setNext(remove->getNext()); 
-       if (tail==remove) tail = tmp;   
-       delete remove;    
-     }
-}
-
-
-#include "PList.h"
-
-int main()
-{
-  PList l;
-
-  l.append(new String("Object"));
-  l.append(new Double(1.50));
-  l.append(new Integer(5);
-
-  PList::iterator it = l.begin();
-  while(!it.end())
-    {
-      Object *o = it.getObject();
-
-      printf("Next thing in list: ");
-      o->print();
-      printf("\n");
-
-      it.increment();
-    }
-
-  return 0;
-}
-
 
