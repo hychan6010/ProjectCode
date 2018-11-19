c735760f73a6f2c7313a1d615746697727e91635
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-11-19 00:53:59 -0500

    Create IntInf.cpp

---
+++ lib/IntInf.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+class IntInf {
+    private:
+      int x;
+      bool pos_inf;
+      bool neg_inf;
+      
+   public: 
+      IntInf(
+    };
+    
+    IntInf operator
+    
+  
