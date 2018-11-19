f77805b8e8ad295a63b335769d3a56a048257de3
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-11-19 18:46:18 -0500

    Create IntInf.h

---
+++ include/IntInf.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+class IntInf {
+    private:
+      int number;
+      bool pos_inf;
+      bool neg_inf;
+      
+   public: 
+      IntInf(int x);
+    };
+    
+IntInf operator=(const IntInf &rhs) {
+
+}
+      
+IntInf operator+(const IntInf &rhs) {
+
+}
+
+IntInf operator-(const IntInf &rhs) {
+
+}
+
+
+IntInf operator*(const IntInf &rhs) {
+
+}
+
+
+IntInf operator/(const IntInf &rhs) {
+
+}
+
+IntInf operator==(const IntInf &rhs) {
+
+}
+
+IntInf operator>(const IntInf &rhs) {
+
+}
+
+IntInf operator<(const IntInf &rhs) {
+
+}
