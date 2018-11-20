0ea6dcbbb71cfe5eae5e5986628ba6820c6a5b8d
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-11-20 13:04:46 -0500

    Update IntInf.h

--- include/IntInf.h
+++ include/IntInf.h
@@ -7,14 +7,16 @@ class IntInf {
    public: 
       bool pos_inf();
       bool neg_inf();
-      IntInf(int x);
+      int result;
     };
     
-if(pos_inf == true){
-    x 
-}   
-if(neg_inf == true){
-    x 
+IntInf(int x)::int number{
+    if(pos_inf == -1){
+        return true;
+    }
+    else if(neg_inf == -2){
+        return true;
+    }
 }
 IntInf operator=(const IntInf &rhs) {
         this = &rhs; 
@@ -22,19 +24,61 @@ IntInf operator=(const IntInf &rhs) {
 }
       
 IntInf operator+(const IntInf &rhs) {
-        this += &rhs;
-        return *this; 
-        if(this == pos_inf){
-            *this 
+        rhs + rhs.number = result;
+        if(rhs || rhs.number == -1){    //if one equals positive infinity return infinity
+            return true;
+        }
+        else if(rhs || rhs.number == -2){ //if one equals negative infinity return negative infinity
+            return true;
+        }
+        return result;                      // return result
 }
 
 IntInf operator-(const IntInf &rhs) {
-
+        rhs - rhs.number = result;
+        if(rhs == -1){              //if only the first number was infinity, return infinity
+            return true;
+        }
+        else if(rhs == -2){         //if only the first number was negative infinity, return negative infinity
+            return true;
+        }
+        if(rhs && rhs.number == -1){    // if both are infinity, return infinity
+            return true;
+        }
+        if(rhs.number == -1){           //if number minus infinity return negative infinity
+            neg_inf == true;
+        }
+        return result;                  // result result
 }
 
 
 IntInf operator*(const IntInf &rhs) {
-
+        rhs * rhs.number = result;
+        if(rhs || rhs.number == -1 && rhs || rhs.number == 0){  //if one is infinity and the other zero, return 0
+            result = 0;
+        }
+        if(rhs || rhs.number == -2 && rhs || rhs.number == 0){  //if one is negative infinity and the other zero, 
+            result = 0;
+        }
+        if(rhs || rhs.number == -1){        // if one or other is infinity, return infinity
+            return true;
+        }
+         if(rhs || rhs.number == -2){       //if one or the other is negative infinity, return negative infinity
+            return true;
+        }
+        if(rhs && rhs.number == -1){        //if both are infinity, return infinity
+         return true;
+        }
+        if(rhs && rhs.number == -2){         // if both are negative infinity, return negative infinity   
+         pos_inf == true;;
+        }
+        if(rhs == -1 && rhs.number == -2){    //if one is infinity and the other negative infinity return negative infinity    
+         neg_inf == true;;
+        }
+        if(rhs == -2 && rhs.number == -1){    // if one is negative infinity and the other infinity, return negative infinity
+         neg_inf == true;;
+        }
+        return result;
 }
 
 
