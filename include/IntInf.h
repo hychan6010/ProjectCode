0b00b7fb1a7d94ee155994fff28e50580776816a
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-11-19 20:47:50 -0500

    Update IntInf.h

--- include/IntInf.h
+++ include/IntInf.h
@@ -3,19 +3,29 @@
 class IntInf {
     private:
       int number;
-      bool pos_inf;
-      bool neg_inf;
-      
+
    public: 
+      bool pos_inf();
+      bool neg_inf();
       IntInf(int x);
     };
     
+if(pos_inf == true){
+    x 
+}   
+if(neg_inf == true){
+    x 
+}
 IntInf operator=(const IntInf &rhs) {
-
+        this = &rhs; 
+        return *this
 }
       
 IntInf operator+(const IntInf &rhs) {
-
+        this += &rhs;
+        return *this; 
+        if(this == pos_inf){
+            *this 
 }
 
 IntInf operator-(const IntInf &rhs) {
