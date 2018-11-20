a380fae10f8a10b6ecddb23bc109cd99b5d175de
hychan6010 <42948014+hychan6010@users.noreply.github.com>
2018-11-20 14:12:13 -0500

    Update IntInf.h

--- include/IntInf.h
+++ include/IntInf.h
@@ -7,15 +7,17 @@ class IntInf {
    public: 
       bool pos_inf();
       bool neg_inf();
-      int result;
     };
     
 IntInf(int x)::int number{
-    if(pos_inf == -1){
-        return true;
+    if(x == -1){
+       pos_inf=true;
     }
-    else if(neg_inf == -2){
-        return true;
+    else if(x==-2){
+        neg_inf=true;
+    }
+    else{
+        number = x; 
     }
 }
 IntInf operator=(const IntInf &rhs) {
@@ -24,66 +26,120 @@ IntInf operator=(const IntInf &rhs) {
 }
       
 IntInf operator+(const IntInf &rhs) {
-        rhs + rhs.number = result;
-        if(rhs || rhs.number == -1){    //if one equals positive infinity return infinity
-            return true;
+        IntInf result;
+        result = number + rhs.number
+        if(pos_inf || rhs.pos_inf){    //if one equals positive infinity return infinity
+            return result.pos_inf = true;
         }
-        else if(rhs || rhs.number == -2){ //if one equals negative infinity return negative infinity
-            return true;
+        else if(neg_inf && rhs.neg_inf){ //if one equals negative infinity return negative infinity
+            return result.neg_inf = true;
         }
+        else{
         return result;                      // return result
-}
+        }
+}    
 
 IntInf operator-(const IntInf &rhs) {
-        rhs - rhs.number = result;
-        if(rhs == -1){              //if only the first number was infinity, return infinity
-            return true;
+        IntInf result;
+        result = number - rhs.number;
+        if(pos_inf){              // infinity - number
+            return result.pos_inf;
         }
-        else if(rhs == -2){         //if only the first number was negative infinity, return negative infinity
-            return true;
+        else if(rhs.pos_inf){         //number - infinity
+           return result.neg_inf;
         }
-        if(rhs && rhs.number == -1){    // if both are infinity, return infinity
-            return true;
+        else if(pos_inf && rhs.pos_inf){    // infinity- infinity
+            return result.pos_inf;
         }
-        if(rhs.number == -1){           //if number minus infinity return negative infinity
-            neg_inf == true;
+        else if(neg_inf){           //if negative infinity - number
+            return result.neg_inf;
         }
-        return result;                  // result result
+        else if(rhs.neg_inf){           //if number - (-infinity)
+            return result.pos_inf;
+        }
+        else if(neg_inf && rhs.neg_inf){    // -infinity - (-infinity)
+            return result.pos_inf;
+        }
+        else {
+        return result;                  // return result
+        }
 }
 
 
 IntInf operator*(const IntInf &rhs) {
-        rhs * rhs.number = result;
-        if(rhs || rhs.number == -1 && rhs || rhs.number == 0){  //if one is infinity and the other zero, return 0
-            result = 0;
+        IntInf result;
+        result = number  * rhs.number;
+    
+       if(pos_inf && rhs.number == 0){    // infinity * 0
+            return result.number = 0;
         }
-        if(rhs || rhs.number == -2 && rhs || rhs.number == 0){  //if one is negative infinity and the other zero, 
-            result = 0;
+        else if(number == 0 && rhs.pos_inf){ // 0 * infinity
+           return result.number = 0;
         }
-        if(rhs || rhs.number == -1){        // if one or other is infinity, return infinity
-            return true;
+        else if(neg_inf && rhs.number == 0){ // -infinity * 0
+           return result.number = 0;
         }
-         if(rhs || rhs.number == -2){       //if one or the other is negative infinity, return negative infinity
-            return true;
+         else if(number == 0 && rhs.neg_inf){ // 0 * -infinity
+           return result.number = 0;
         }
-        if(rhs && rhs.number == -1){        //if both are infinity, return infinity
-         return true;
+        else if(pos_inf || rhs.pos_inf){  // infinity * number or number * infinity
+           return result.pos_inf;
         }
-        if(rhs && rhs.number == -2){         // if both are negative infinity, return negative infinity   
-         pos_inf == true;;
+        else if(neg_inf || neg.pos_inf){  // -infinity * number or number * -infinity
+           return result.neg_inf;
         }
-        if(rhs == -1 && rhs.number == -2){    //if one is infinity and the other negative infinity return negative infinity    
-         neg_inf == true;;
+        else if (pos_inf && rhs.pos_inf){  // infinity * infinity
+            return result.pos_inf;
         }
-        if(rhs == -2 && rhs.number == -1){    // if one is negative infinity and the other infinity, return negative infinity
-         neg_inf == true;;
+        else if (neg_inf && rhs.neg_inf){   //-infinity * -infinity
+            return result.pos_inf;
         }
-        return result;
+        else if (pos_inf && rhs.neg_inf){   //infinity * -infinity
+            return result.neg_inf;
+        }
+        else if (pos_inf && rhs.neg_inf){   //infinity * -infinity
+            return result.neg_inf;
+        }
+        else {
+        return result;                  // return result
+        }
 }
 
 
 IntInf operator/(const IntInf &rhs) {
-
+        IntInf result;
+        result = number  / rhs.number;
+    
+       if(number && rhs.pos_inf == 0){    // number / infinity
+            return result.number = 0;
+        }
+       else if(number && rhs.neg_inf == 0){    // number / -infinity
+            return result.number = 0;
+        }
+        else if(pos_inf && rhs.number){    // infinity / number
+            return result.pos_inf;
+        }
+        else if(neg_inf && rhs.number){    // -infinity / number
+            return result.neg_inf;
+        }
+        else if(neg_inf && rhs.neg_inf){    // -infinity / -infinity
+            return result.pos_inf;
+        }
+       else if(pos_inf && rhs.pos_inf){    // infinity / infinity
+            return result.pos_inf;
+        }
+       else if(pos_inf && rhs.neg_inf){    // infinity / -infinity
+            return result.neg_inf;
+        }
+       else if(neg_inf && rhs.pos_inf){    // -infinity / infinity
+            return result.neg_inf;
+        }
+        else if(number||rhs.number ==0){  // number / 0
+           printf("undefined");
+        }
+        else {
+        return result;                  // return result
+        }
 }
 
 IntInf operator==(const IntInf &rhs) {
